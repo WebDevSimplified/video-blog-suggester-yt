18606dc0869f8329e2fe8cee693efac597d695d1
Web Dev Simplified <kyle@webdevsimplified.com>
2026-07-07 09:12:53 -0500

    Add Github Auth

---
+++ src/lib/auth/config.ts
@@ -0,0 +1,27 @@
+import { betterAuth } from "better-auth/minimal"
+import { drizzleAdapter } from "@better-auth/drizzle-adapter/relations-v2"
+import { nextCookies } from "better-auth/next-js"
+import { serverEnv } from "@/data/serverEnv"
+import { db } from "@/db/db"
+import * as schema from "@/db/schema"
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema,
+  }),
+  advanced: {
+    database: {
+      generateId: "uuid",
+    },
+  },
+  socialProviders: {
+    github: {
+      clientId: serverEnv.GITHUB_CLIENT_ID,
+      clientSecret: serverEnv.GITHUB_CLIENT_SECRET,
+    },
+  },
+  plugins: [nextCookies()],
+})
+
+export type Session = typeof auth.$Infer.Session
